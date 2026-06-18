74f46b3c5476fefca9e0c96d9afbe4920963896a
esmaeilisadat-tech <esmaeili.sadat@gmail.com>
2026-06-18 13:12:05 +0200

    Clean up repository, remove unused scripts and notebooks

--- print_final_pdf.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --headless --print-to-pdf="%CD%\WorldCup2026_Mathematical_Deep_Dive.pdf" "file:///%CD%\Mathematical_Deep_Dive.html"
--- test_pandas.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import traceback
-df = pd.DataFrame({'A':[1]})
-try:
-    df['B'], df['C'] = 2, 3
-    print("Tuple unpack works")
-except Exception as e:
-    print("Tuple unpack failed:", traceback.format_exc())
